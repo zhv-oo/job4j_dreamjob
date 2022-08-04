1de52805ebdfe506b2ab151bd85cbfcd882b648d
zhv-oo <zhitenev.oleg@yandex.ru>
2022-08-04 10:27:00 +0700

    3. Тестирование базы данных. Liquibase H2 [#504862 #328521]

--- db/scripts/update_001.sql
+++ db/scripts/update_001.sql
@@ -1,4 +1,4 @@
-CREATE TABLE post (
+CREATE TABLE IF NOT EXISTS post (
    id SERIAL PRIMARY KEY,
    name text,
    description text,
@@ -7,7 +7,7 @@ CREATE TABLE post (
    city_id INT
 );
 
-CREATE TABLE candidate (
+CREATE TABLE IF NOT EXISTS candidate (
    id SERIAL PRIMARY KEY,
    name text,
    description text,
