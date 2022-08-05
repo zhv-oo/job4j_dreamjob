89da1414a234619f258787b1ba704f82ac8bf15f
zhv-oo <zhitenev.oleg@yandex.ru>
2022-08-05 19:50:44 +0700

    4. Многопоточность в базе данных [#504860]

---
+++ src/main/java/ru/job4j/dreamjob/controller/UsersRegController.java
@@ -0,0 +1,51 @@
+package ru.job4j.dreamjob.controller;
+
+import net.jcip.annotations.ThreadSafe;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.job4j.dreamjob.model.User;
+import ru.job4j.dreamjob.services.UserService;
+
+import java.util.Optional;
+
+@ThreadSafe
+@Controller
+public class UsersRegController {
+    private final UserService userService;
+
+    public UsersRegController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/formRegistrationUser")
+    public String addUser(Model model) {
+        model.addAttribute("user", new User(0, "Введите почту", "Введите имя"));
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String registration(Model model, @ModelAttribute User user) {
+        if (user != null) {
+            Optional<User> regUser = Optional.ofNullable(userService.add(user));
+            if (regUser.isEmpty()) {
+                model.addAttribute("message", "Пользователь с такой почтой уже существует");
+                return "redirect:/fail";
+            }
+        }
+
+        return "redirect:/success";
+    }
+
+    @GetMapping("/success")
+    public String success() {
+        return "success";
+    }
+
+    @GetMapping("/fail")
+    public String fail() {
+        return "fail";
+    }
+}
---
+++ src/main/java/ru/job4j/dreamjob/store/UserDBStore.java
@@ -0,0 +1,42 @@
+package ru.job4j.dreamjob.store;
+
+import net.jcip.annotations.ThreadSafe;
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Repository;
+import ru.job4j.dreamjob.model.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+@ThreadSafe
+@Repository
+public class UserDBStore {
+    private final BasicDataSource pool;
+    private static Logger logger = Logger.getLogger(CandidateDbStore.class);
+
+    public UserDBStore(BasicDataSource pool) {
+        this.pool = pool;
+    }
+
+    public User add(User user) {
+        try (Connection cn = pool.getConnection();
+             PreparedStatement ps =  cn.prepareStatement("INSERT INTO users(email, password) VALUES (?, ?)",
+                     PreparedStatement.RETURN_GENERATED_KEYS)
+        ) {
+            ps.setString(1, user.getEmail());
+            ps.setString(2, user.getPassword());
+            ps.execute();
+            try (ResultSet id = ps.getGeneratedKeys()) {
+                if (id.next()) {
+                    user.setId(id.getInt(1));
+                }
+            }
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            user = null;
+        }
+        return user;
+    }
+}
