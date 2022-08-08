f566ac668f05b3c346801afe7efe3757a3ea3885
zhv-oo <zhitenev.oleg@yandex.ru>
2022-08-08 17:21:59 +0700

    3. Filter [#504865]

---
+++ src/main/java/ru/job4j/dreamjob/filter/AuthFilter.java
@@ -0,0 +1,30 @@
+package ru.job4j.dreamjob.filter;
+
+import org.springframework.stereotype.Component;
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class AuthFilter implements Filter {
+
+    @Override
+    public void doFilter(
+            ServletRequest request,
+            ServletResponse response,
+            FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        String uri = req.getRequestURI();
+        if (uri.endsWith("loginPage") || uri.endsWith("login")) {
+            chain.doFilter(req, res);
+            return;
+        }
+        if (req.getSession().getAttribute("user") == null) {
+            res.sendRedirect(req.getContextPath() + "/loginPage");
+            return;
+        }
+        chain.doFilter(req, res);
+    }
+}
