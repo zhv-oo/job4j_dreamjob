84fcb3107c148da432b9e7da6a8a8c86a7761b09
zhv-oo <zhitenev.oleg@yandex.ru>
2022-08-10 21:04:14 +0700

    3. Filter [#504865 #329974]

--- src/main/java/ru/job4j/dreamjob/filter/AuthFilter.java
+++ src/main/java/ru/job4j/dreamjob/filter/AuthFilter.java
@@ -17,11 +17,8 @@ public class AuthFilter implements Filter {
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse res = (HttpServletResponse) response;
         String uri = req.getRequestURI();
-        if (uri.endsWith("RegistrationUser") || uri.endsWith("registration") || uri.endsWith("fail")) {
-            chain.doFilter(req, res);
-            return;
-        }
-        if (uri.endsWith("loginPage") || uri.endsWith("login")) {
+        if (uri.endsWith("loginPage") || uri.endsWith("login") || uri.endsWith("RegistrationUser")
+                || uri.endsWith("registration") || uri.endsWith("fail")) {
             chain.doFilter(req, res);
             return;
         }
