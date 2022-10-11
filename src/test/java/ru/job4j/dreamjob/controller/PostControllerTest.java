925e99af7200601a421118dc755859fc2ff8c033
zhv-oo <zhitenev.oleg@yandex.ru>
2022-10-11 21:48:24 +0700

    2. Тестируем PostController [#504867]

---
+++ src/test/java/ru/job4j/dreamjob/controller/PostControllerTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.dreamjob.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.ui.Model;
+import ru.job4j.dreamjob.model.Post;
+import ru.job4j.dreamjob.services.CityService;
+import ru.job4j.dreamjob.services.PostService;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+class PostControllerTest {
+    @Test
+    public void whenPosts() {
+        List<Post> posts = Arrays.asList(
+                new Post(1, "New post"),
+                new Post(2, "New post")
+        );
+        Model model = mock(Model.class);
+        PostService postService = mock(PostService.class);
+        when(postService.findAll()).thenReturn(posts);
+        CityService cityService = mock(CityService.class);
+        PostController postController = new PostController(
+                postService,
+                cityService
+        );
+        String page = postController.posts(model, new MockHttpSession());
+        verify(model).addAttribute("posts", posts);
+        assertThat(page).isEqualTo("posts");
+    }
+}
